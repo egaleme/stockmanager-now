471b7a56191b2b0dd71462995a4089aaac5e88ac
Aleme Gabriel <egaleme@gmail.com>
2016-08-09 14:31:45 +0100

    smtp settings updated

--- imports/startup/server/smtp.js
+++ imports/startup/server/smtp.js
@@ -1,15 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 
-let smtpSettings;
 Meteor.startup(function() {
-	smtpSettings = {
-		username: 'stockdiaryapp',
-		password: '200owina07',
-		server: 'smtp.gmail.com',
-		port: 587
-	};
 
-	process.env.MAIL_URL = "smtp://stockdiaryapp%40gmail.com:200owina07@smtp.gmail.com:587";
-			
+	process.env.MAIL_URL = "smtp://stockdiaryapp%40gmail.com:200owina07@smtp.gmail.com:587";	
 
 })
